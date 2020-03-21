18d0b0a5dfd1728d5ef24e7965153c2ee219c1cc
codewithsmit <codewithsmit@gmail.com>
2020-03-21 15:19:29 +0530

    First

--- headbranch/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from . models import HeadBranch, Branch, Account, HeadBranch_Transaction, Branch_Transaction
-# Register your models here.
-
-admin.site.register(HeadBranch)
-admin.site.register(Branch)
-admin.site.register(Account)
-admin.site.register(HeadBranch_Transaction)
-admin.site.register(Branch_Transaction)
